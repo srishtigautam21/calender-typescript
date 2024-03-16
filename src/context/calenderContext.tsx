f513e305f39d62a4d848a6be20b2c4a51bd03ed0
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-03-16 12:04:22 +0530

    Added typing for context

--- src/context/calenderContext.tsx
+++ src/context/calenderContext.tsx
@@ -1,8 +1,29 @@
 import { createContext, useContext, useReducer } from "react";
 
-const CalenderContext = createContext({});
+type ContextProviderProps = {
+  children: React.ReactNode;
+};
+type InitialStateType = {
+  year: number;
+  month: number;
+  date: number;
+};
+type MonthActionType = {
+  type: string;
+  payload?: number;
+};
 
-const CalenderProvider = ({ children }: any) => {
+type MonthContextType = {
+  state: InitialStateType | null;
+  // action: MonthActionType | null;
+  dispatch: React.DispatchWithoutAction;
+  monthArr: string[];
+};
+// type ReducerFuncType = {};
+
+const CalenderContext = createContext<MonthContextType | null>(null);
+
+const CalenderProvider = ({ children }: ContextProviderProps) => {
   const monthArr = [
     "January",
     "Feburary",
@@ -17,16 +38,9 @@ const CalenderProvider = ({ children }: any) => {
     "November",
     "December",
   ];
-  type InitialStateType = {
-    year: number;
-    month: number;
-    date: number;
-  };
-  type MonthActionType = {
-    type: string;
-  };
+
   const dateString = new Date(); //Gives date as afull string "Fri Jun 17 2022 10:54:59 GMT+0100 (British Summer Time)"
-  const initialState: any = {
+  const initialState = {
     year: dateString.getFullYear(), // current year 2024
     month: dateString.getMonth() + 1, //gives index of month from 0-11,
     date: dateString.getDate(),
@@ -47,7 +61,7 @@ const CalenderProvider = ({ children }: any) => {
     }
   };
 
-  const [state, dispatch] = useReducer<InitialStateType | any>(
+  const [state, dispatch] = useReducer<InitialStateType>(
     reducerFunction,
     initialState
   );
